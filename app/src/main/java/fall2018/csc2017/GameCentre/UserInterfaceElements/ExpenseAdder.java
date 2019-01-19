64f82f887e3af8787e4ed12ab3b8a30a1028cf4c
wasimzuh <zuhab.wasim@mail.utoronto.ca>
2019-01-19 15:23:54 -0500

    Added access to the first two activities and started working on the Expense, and Event objects.

---
+++ app/src/main/java/fall2018/csc2017/GameCentre/UserInterfaceElements/ExpenseAdder.java
@@ -0,0 +1,44 @@
+package fall2018.csc2017.GameCentre.UserInterfaceElements;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import fall2018.csc2017.GameCentre.DataManagers.User;
+import fall2018.csc2017.GameCentre.R;
+
+public class ExpenseAdder extends AppCompatActivity {
+
+    /**
+     * The current logged in user
+     */
+    private User curUser;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        //Make general later
+//
+//        //Fetches the high scores
+//        //Checks to see if the file is empty, if so, create a new empty file with a default scoreboard, sliding tiles
+//        checkIfEmpty(LeaderboardActivity.SLIDING_SAVE_FILENAME, false);
+//        //Checks to see if the file is empty, if so, create a new empty file with a default scoreboard, minesweeper
+//        checkIfEmpty(LeaderboardActivity.MINESWEEPER_SAVE_FILENAME, false);
+//        //Checks to see if the file is empty, if so, create a new empty file with a default scoreboard, PowersPlus
+//        checkIfEmpty(LeaderboardActivity.POWERS_SAVE_FILENAME, true);
+//
+//        //Reinitialize difficulty
+//        difficulty = null;
+//
+        //Fetches the current user
+        curUser = (User) getIntent().getSerializableExtra("CurUser");
+        //Sets the view
+        setContentView(R.layout.activity_expense_adder);
+//        //Adds the necessary buttons
+//        addCreateEventButtonListener();
+//        addMineSweeperButtonListener();
+//        addPowersPlusButtonListener();
+//        addLeaderBoardButtonListener();
+//        addDifficultyListener();
+//        getAddLogOutButtonListener();
+    }
+}
--- app/src/main/java/fall2018/csc2017/GameCentre/UserInterfaceElements/LaunchCentre.java
+++ app/src/main/java/fall2018/csc2017/GameCentre/UserInterfaceElements/LaunchCentre.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import fall2018.csc2017.GameCentre.DataManagers.FileManager;
 import fall2018.csc2017.GameCentre.R;
 import fall2018.csc2017.GameCentre.DataManagers.User;
@@ -20,10 +23,10 @@ import fall2018.csc2017.GameCentre.DataManagers.UserManager;
  */
 public class LaunchCentre extends AppCompatActivity {
 
-    /**
-     * The list of games implemented so far
-     */
-    private final String gamesList[] = {"SlidingTiles", "MineSweeper", "PowersPlus"};
+//    /**
+//     * The list of games implemented so far
+//     */
+    //private final String gamesList[] = {"SlidingTiles", "MineSweeper", "PowersPlus"};
 
     /**
      * The main save for users.
@@ -59,172 +62,183 @@ public class LaunchCentre extends AppCompatActivity {
         }
 
         //Change the view and create the buttons
-        setContentView(R.layout.activity_gamelauncher);
-        addSignUpButtonListener();
-        addLoginButtonListener();
+        setContentView(R.layout.activity_email_launcher);
+        //addSignUpButtonListener();
+        //addLoginButtonListener();
+        addEnterButtonListener();
     }
 
-    /**
-     * Activate the Sign up button.
-     */
-    private void addSignUpButtonListener() {
-        Button signUpButton = findViewById(R.id.SignUpButton);
-        //This handles the click of the button
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Create the two text fields and get the username and password from them
-                EditText passwordText = findViewById(R.id.passwordText);
-                EditText signUpText = findViewById(R.id.userNameText);
-                String password = passwordText.getText().toString();
-                String username = signUpText.getText().toString();
-
-                //Create the potential new user
-                User potentialUser = new User(username, password);
-
-                //Load the database from file
-                Activity host = (Activity) v.getContext();
-                userManager = userFileManager.loadFromFile(USER_SAVE_FILENAME, host);
-
-                //Check if this user has already signed up
-                if (!userManager.hasUser(potentialUser) && !username.isEmpty() && !password.isEmpty()) {
-                    //Create a new saved games file for this new user
-                    potentialUser.createSavedGames();
-                    userManager.addUser(potentialUser);
-                    switchToGameSelector(potentialUser);
-                } else {
-                    //Display errors
-                    if (username.isEmpty()){
-                        //Blank username
-                        makeToastBlankUsername();
-                    }else if (password.isEmpty()){
-                        //Blank password
-                        makeToastBlankPassword();
-                    }else{
-                        //Displays an error when signing up if the account already exists
-                        String message = "This user already exists!";
-                        makeToastSignUpErrorText(message);
-                    }
-                }
-
-                //Save the updated database to file
-                userFileManager.saveToFile(USER_SAVE_FILENAME, host, userManager);
-            }
-        });
-    }
+//    /**
+//     * Activate the Sign up button.
+//     */
+//    private void addSignUpButtonListener() {
+//        Button signUpButton = findViewById(R.id.SignUpButton);
+//        //This handles the click of the button
+//        signUpButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //Create the two text fields and get the username and password from them
+//                EditText passwordText = findViewById(R.id.passwordText);
+//                EditText signUpText = findViewById(R.id.userNameText);
+//                String password = passwordText.getText().toString();
+//                String username = signUpText.getText().toString();
+//
+//                //Create the potential new user
+//                User potentialUser = new User(username, password);
+//
+//                //Load the database from file
+//                Activity host = (Activity) v.getContext();
+//                userManager = userFileManager.loadFromFile(USER_SAVE_FILENAME, host);
+//
+//                //Check if this user has already signed up
+//                if (!userManager.hasUser(potentialUser) && !username.isEmpty() && !password.isEmpty()) {
+//                    //Create a new saved games file for this new user
+//                    potentialUser.createSavedGames();
+//                    userManager.addUser(potentialUser);
+//                    switchToGameSelector(potentialUser);
+//                } else {
+//                    //Display errors
+//                    if (username.isEmpty()){
+//                        //Blank username
+//                        Toast.makeText(getApplicationContext(),
+//                                "Please enter an email!",
+//                                Toast.LENGTH_SHORT).show();
+////                    }else if (password.isEmpty()){
+////                        //Blank password
+////                        makeToastBlankPassword();
+//                    }else{
+//                        //Displays an error when signing up if the account already exists
+//                        String message = "This user already exists!";
+//                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//
+//                //Save the updated database to file
+//                userFileManager.saveToFile(USER_SAVE_FILENAME, host, userManager);
+//            }
+//        });
+//    }
 
     /**
      * Activate the login button.
      */
-    private void addLoginButtonListener() {
-        Button loginButton = findViewById(R.id.LogInButton);
+    private void addEnterButtonListener() {
+        Button enterButton = findViewById(R.id.enterButton);
         //This handles the click of the button
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Create the two text fields and get the username and password from them
-                EditText passwordText = findViewById(R.id.passwordText);
-                EditText signUpText = findViewById(R.id.userNameText);
+                //EditText passwordText = findViewById(R.id.passwordText);
+                EditText signUpText = findViewById(R.id.emailText);
                 String username = signUpText.getText().toString();
-                String password = passwordText.getText().toString();
+                //String password = passwordText.getText().toString();
 
                 //Create the potential logged in user
-                User potentialLogin = new User(username, password);
+                User potentialLogin = new User(username, "");
 
                 //Load the database
                 Activity host = (Activity) v.getContext();
                 userManager = userFileManager.loadFromFile(USER_SAVE_FILENAME, host);
 
-                if (userManager.hasUser(potentialLogin) && !username.isEmpty() && !password.isEmpty()) {
-                    //Compare if passwords are correct
-                    if (comparePasswords(potentialLogin)) {
-                        //Switches to the game selector if the password entered is correct
-                        //The user already exists so no need to create a new saved games file
-                        User curUser = userManager.getUser(potentialLogin);
+                if (userManager.hasUser(potentialLogin) && !username.isEmpty()) {
 
-                        //Adds new games to the user if they signed up before those new games were added
-                        fixOldUser(curUser);
+                    //Switches to the game selector if the password entered is correct
+                    //The user already exists so no need to create a new saved games file
+                    User curUser = userManager.getUser(potentialLogin);
 
-                        switchToGameSelector(curUser);
-                    } else {
-                        String message = "Your password is incorrect!";
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-                    }
+                    //Adds new games to the user if they signed up before those new games were added
+                    //fixOldUser(curUser);
+
+                    switchToGameSelector(curUser);
+
                 } else {
+                    Pattern pattern = Pattern.compile("^[A-Za-z0-9.]*@[A-Za-z0-9.]*\\.[A-Za-z0-9]*$");
+                    Matcher m = pattern.matcher(username);
                     //Display errors
                     if (username.isEmpty()){
                         //Blank username
-                        makeToastBlankUsername();
-                    }else if (password.isEmpty()){
-                        //Blank password
-                        makeToastBlankPassword();
+                        Toast.makeText(getApplicationContext(),
+                                "Please enter an email!",
+                                Toast.LENGTH_SHORT).show();
+                    }else if (!m.find()){
+                        String message = "Invalid email!";
+                        Toast.makeText(getApplicationContext(),
+                                message,
+                                Toast.LENGTH_SHORT).show();
                     }else{
-                        //Displays an error when signing up and the account already exists
-                        String message = "This user does not exist!";
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                        userManager.addUser(potentialLogin);
+                        switchToGameSelector(potentialLogin);
+                        //Save the updated database to file
+                        userFileManager.saveToFile(USER_SAVE_FILENAME, host, userManager);
                     }
+
+
                 }
+
+
             }
         });
+
     }
 
-    /**
-     * Adds new games to an old user
-     *
-     * @param curUser The current logged in user
-     */
-    private void fixOldUser(User curUser) {
-        //Checks to see what games are missing if new games are added
-        if (curUser.userSavedGames.size() < gamesList.length){
-            for (String gameType : gamesList){
-                if (!curUser.userSavedGames.containsKey(gameType)){
-                    curUser.addSave(gameType);
-                }
-            }
-        }
-    }
+//    /**
+//     * Adds new games to an old user
+//     *
+//     * @param curUser The current logged in user
+//     */
+//    private void fixOldUser(User curUser) {
+//        //Checks to see what games are missing if new games are added
+//        if (curUser.userSavedGames.size() < gamesList.length){
+//            for (String gameType : gamesList){
+//                if (!curUser.userSavedGames.containsKey(gameType)){
+//                    curUser.addSave(gameType);
+//                }
+//            }
+//        }
+//    }
 
 
-    /**
-     * Compares the password of the current field and the password stored in the database
-     * @param user the current user trying to log in
-     * @return True or False, depending on if the password matches or not
-     */
-    private boolean comparePasswords(User user) {
-        for (User u : userManager) {
-            if (u.getUsername().equals(user.getUsername())) {
-                return u.getPassword().equals(user.getPassword());
-            }
-        }
-        return false;
-    }
+//    /**
+//     * Compares the password of the current field and the password stored in the database
+//     * @param user the current user trying to log in
+//     * @return True or False, depending on if the password matches or not
+//     */
+//    private boolean comparePasswords(User user) {
+//        for (User u : userManager) {
+//            if (u.getUsername().equals(user.getUsername())) {
+//                return u.getPassword().equals(user.getPassword());
+//            }
+//        }
+//        return false;
+//    }
 
-    /**
-     * Display an sign up error
-     */
-    private void makeToastSignUpErrorText(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+//    /**
+//     * Display an sign up error
+//     */
+//    private void makeToastSignUpErrorText(String message) {
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+//    }
 
-    /**
-     * Display a blank username message
-     */
-    private void makeToastBlankUsername() {
-        Toast.makeText(this, "Please enter a username!", Toast.LENGTH_SHORT).show();
-    }
+//    /**
+//     * Display a blank username message
+//     */
+//    private void makeToastBlankUsername() {
+//        Toast.makeText(this, "Please enter a username!", Toast.LENGTH_SHORT).show();
+//    }
 
-    /**
-     * Display a blank password message
-     */
-    private void makeToastBlankPassword() {
-        Toast.makeText(this, "Please enter a password!", Toast.LENGTH_SHORT).show();
-    }
+//    /**
+//     * Display a blank password message
+//     */
+//    private void makeToastBlankPassword() {
+//        Toast.makeText(this, "Please enter a password!", Toast.LENGTH_SHORT).show();
+//    }
 
     /**
      * Switch to the Game selector view to select what game to play.
      */
     private void switchToGameSelector(User currentUser) {
-        Intent tmp = new Intent(this, GameHubActivity.class);
+        Intent tmp = new Intent(this, EventActivity.class);
 
         //Store the current user in the next activity
         tmp.putExtra("CurUser", currentUser);
