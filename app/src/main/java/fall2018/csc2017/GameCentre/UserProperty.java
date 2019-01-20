1c07381337b594c6264da990f7906467a943b9cb
Alex Quach <alex.quach1234@gmail.com>
2019-01-20 00:13:01 -0500

    stuff

---
+++ app/src/main/java/fall2018/csc2017/GameCentre/UserProperty.java
@@ -0,0 +1,27 @@
+package fall2018.csc2017.GameCentre;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserProperty {
+
+    private String email;
+    private List<String> friends;
+
+    public UserProperty(){};
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserProperty(String email, List<String> friends){
+        this.email = email;
+        this.friends = friends;
+    }
+
+    public List<String> getFriends() { return friends; }
+
+    public void addFriend(String email){
+        this.friends.add(email);
+    }
+}
